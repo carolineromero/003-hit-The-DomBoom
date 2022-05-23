cf639f82881651105d55dfb10eccc4d044e8d8ed
caroolromero <carolf5laciba@gmail.com>
2022-05-23 14:43:50 +0200

    ventana 2 mobil

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-var sonido = new audio();
-sonido.src =
-function PlaySound(){
-    bombo.play();
-}
-
-=======
 function playAudio(url) {
-    new Audio(url).play();
-  }
->>>>>>> f9055bad74ac76030d8e5a6584f1664499215805
+  new Audio(url).play();
+}
