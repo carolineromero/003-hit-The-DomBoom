5b6d9fa65d05977b5abc8a9a1c30782ff64682a3
caroolromero <carolf5laciba@gmail.com>
2022-06-03 13:58:51 +0200

    version mvl

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
-function playAudio(url) {
-  new Audio(url).play();
-}
+
+//function playAudio(url) {
+  //new Audio(url).play();
+//}
+
+
+var bombo = new Audio();
+bombo.src = "./audios/bombo.wav";
+
+var crash = new Audio();
+crash.src = "./audios/crash.wav";
+
+var hithat = new Audio();
+hithat.src = "./audios/hit-hat.wav";
+
+var redoblante = new Audio();
+redoblante.src = "./audios/redoblante.wav";
+
+var tom1 = new Audio();
+tom1.src = "./audios/tom1.wav";
+
+var tom2 = new Audio();
+tom2.src = "./audios/tom2.wav";
+
+var tom3 = new Audio();
+tom3.src = "./audios/tom3.wav";
+
+var tom4 = new Audio();
+tom4.src = "./audios/tom4.wav";
